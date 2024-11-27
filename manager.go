15c865d98628b868d4412317aa412543a8dd85d6
arpit529srivastava <arpitsrivastava529@gmail.com>
2024-11-28 02:07:40 +0530

    manager file added for the webapp

---
+++ manager.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+var (
+	websocketUpgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+)
+
+type Manager struct {
+	clients ClientList
+	sync.RWMutex
+}
+
+func NewManager() *Manager {
+	return &Manager{
+		clients: make(ClientList),
+	}
+}
+
+func (m *Manager) serveWS(w http.ResponseWriter, r *http.Request) {
+	log.Println("connection established")
+	// it will upgrade the regular https connection into the websockets
+	conn, err := websocketUpgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	client := NewClient(conn, m)
+	m.addClient(client)
+	// go-routine entered the chats
+	go client.readMessage()
+
+}
+
+func (m *Manager) addClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+	m.clients[client] = true
+}
+func (m *Manager) removeClient(client *Client){
+	m.Lock()
+	defer m.Unlock()
+	if _, ok := m.clients[client]; ok {
+		client.connection.Close()
+		delete(m.clients, client)
+	}
+}
