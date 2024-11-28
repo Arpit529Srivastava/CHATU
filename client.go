2fe06b9bad4d5814d084a8f3652af2f66236c6f1
arpit529srivastava <arpitsrivastava529@gmail.com>
2024-11-28 10:19:56 +0530

    added the egree channel for concurrency

--- client.go
+++ client.go
@@ -11,26 +11,30 @@ type ClientList map[*Client]bool
 type Client struct {
 	connection *websocket.Conn
 	manager    *Manager
+	// we are using egress for avoiding concurrencies write on the connection
+	egress chan []byte
 }
 
 func NewClient(conn *websocket.Conn, manager *Manager) *Client {
 	return &Client{
 		connection: conn,
 		manager:    manager,
+		egress: make(chan []byte),
 	}
 }
+
 // reading and writing the messages
 // websockets only allow one at a time not simultaneously
-func (c *Client) readMessage(){
-	defer func(){
+func (c *Client) readMessage() {
+	defer func() {
 		//cleaning up the connection
 		c.manager.removeClient(c)
 	}()
 	for {
-		messageType, payload, err :=c.connection.ReadMessage()
+		messageType, payload, err := c.connection.ReadMessage()
 
-		if err != nil{
-			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure){
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
 				log.Printf("error while reading message : %v", err)
 			}
 			break
@@ -39,4 +43,24 @@ func (c *Client) readMessage(){
 		log.Println(string(payload))
 	}
 }
+
 // starting with concurrency
+func (c *Client) writeMessage(){
+	defer func(){
+		c.manager.removeClient((c))
+	}()
+	for {
+		select {
+		case message, ok := <-c.egress:
+			if !ok {
+				if err := c.connection.WriteMessage(websocket.CloseMessage, nil); err != nil{
+					log.Println("connection id closed :", err)
+				} 
+				return
+			}
+			if err := c.connection.WriteMessage(websocket.TextMessage, message); err != nil {
+				log.Printf("failed to send message : %v", err)
+			}
+		}
+	}
+}
