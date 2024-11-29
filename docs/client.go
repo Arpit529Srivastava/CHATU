012a15bddc57f58904904caf9b5ca1db78540dd0
arpit529srivastava <arpitsrivastava529@gmail.com>
2024-11-29 12:11:22 +0530

    pushed to deploy

---
+++ docs/client.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+type ClientList map[*Client]bool
+
+type Client struct {
+	connection *websocket.Conn
+	manager *Manager
+	egress chan Event
+	chatroom string
+}
+
+var (
+	pongWait = 10 * time.Second
+	pingInterval = (pongWait * 9) / 10
+)
+
+func NewClient(conn *websocket.Conn, manager *Manager) *Client {
+	return &Client{
+		connection: conn,
+		manager:    manager,
+		egress:     make(chan Event),
+	}
+}
+
+func (c *Client) readMessages() {
+	defer func() {
+		c.manager.removeClient(c)
+	}()
+	c.connection.SetReadLimit(512)
+	if err := c.connection.SetReadDeadline(time.Now().Add(pongWait)); err != nil {
+		log.Println(err)
+		return
+	}
+	c.connection.SetPongHandler(c.pongHandler)
+	for {
+		_, payload, err := c.connection.ReadMessage()
+
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error reading message: %v", err)
+			}
+			break 
+		}
+		var request Event
+		if err := json.Unmarshal(payload, &request); err != nil {
+			log.Printf("error marshalling message: %v", err)
+			break 
+		}
+		if err := c.manager.routeEvent(request, c); err != nil {
+			log.Println("Error handeling Message: ", err)
+		}
+	}
+}
+func (c *Client) pongHandler(pongMsg string) error {
+	// Current time + Pong Wait time
+	log.Println("pong")
+	return c.connection.SetReadDeadline(time.Now().Add(pongWait))
+}
+
+// writeMessages is a process that listens for new messages to output to the Client
+func (c *Client) writeMessages() {
+	// Create a ticker that triggers a ping at given interval
+	ticker := time.NewTicker(pingInterval)
+	defer func() {
+		ticker.Stop()
+		// Graceful close if this triggers a closing
+		c.manager.removeClient(c)
+	}()
+
+	for {
+		select {
+		case message, ok := <-c.egress:
+			// Ok will be false Incase the egress channel is closed
+			if !ok {
+				// Manager has closed this connection channel, so communicate that to frontend
+				if err := c.connection.WriteMessage(websocket.CloseMessage, nil); err != nil {
+					// Log that the connection is closed and the reason
+					log.Println("connection closed: ", err)
+				}
+				// Return to close the goroutine
+				return
+			}
+
+			data, err := json.Marshal(message)
+			if err != nil {
+				log.Println(err)
+				return // closes the connection, should we really
+			}
+			// Write a Regular text message to the connection
+			if err := c.connection.WriteMessage(websocket.TextMessage, data); err != nil {
+				log.Println(err)
+			}
+			log.Println("sent message")
+		case <-ticker.C:
+			log.Println("ping")
+			// Send the Ping
+			if err := c.connection.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				log.Println("writemsg: ", err)
+				return // return to break this goroutine triggeing cleanup
+			}
+		}
+
+	}
+}
---
+++ docs/frontend/chat.js
@@ -0,0 +1,131 @@
+var selectedchat = "general";
+
+class Event {
+    constructor(type, payload) {
+        this.type = type;
+        this.payload = payload;
+    }
+}
+
+class SendMessageEvent {
+    constructor(message, from) {
+        this.message = message;
+        this.from = from;
+    }
+}
+
+class NewMessageEvent {
+    constructor(message, from, sent) {
+        this.message = message;
+        this.from = from;
+        this.sent = sent;
+    }
+}
+
+function routeEvent(event) {
+    if (event.type === undefined) {
+        alert("no 'type' field in event");
+    }
+    switch (event.type) {
+        case "new_message":
+            const messageEvent = Object.assign(new NewMessageEvent, event.payload);
+            appendChatMessage(messageEvent);
+            break;
+        default:
+            alert("unsupported message type");
+            break;
+    }
+}
+
+function appendChatMessage(messageEvent) {
+    var date = new Date(messageEvent.sent);
+    const formattedMsg = `${date.toLocaleString()}: ${messageEvent.message}`;
+    textarea = document.getElementById("chatmessages");
+    textarea.innerHTML = textarea.innerHTML + "\n" + formattedMsg;
+    textarea.scrollTop = textarea.scrollHeight;
+}
+
+class ChangeChatRoomEvent {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+function changeChatRoom() {
+    var newchat = document.getElementById("chatroom");
+    if (newchat != null && newchat.value != selectedchat) {
+        selectedchat = newchat.value;
+        header = document.getElementById("chat-header").innerHTML = "Currently in chat: " + selectedchat;
+
+        let changeEvent = new ChangeChatRoomEvent(selectedchat);
+        sendEvent("change_room", changeEvent);
+        textarea = document.getElementById("chatmessages");
+        textarea.innerHTML = `You changed room into: ${selectedchat}`;
+    }
+    return false;
+}
+
+function sendMessage() {
+    var newmessage = document.getElementById("message");
+    if (newmessage != null) {
+        let outgoingEvent = new SendMessageEvent(newmessage.value, "percy");
+        sendEvent("send_message", outgoingEvent)
+    }
+    return false;
+}
+
+function sendEvent(eventName, payload) {
+    const event = new Event(eventName, payload);
+    conn.send(JSON.stringify(event));
+}
+
+function login() {
+    let formData = {
+        "username": document.getElementById("username").value,
+        "password": document.getElementById("password").value
+    }
+    fetch("login", {
+        method: 'post',
+        body: JSON.stringify(formData),
+        mode: 'cors',
+    }).then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw 'unauthorized';
+        }
+    }).then((data) => {
+        connectWebsocket(data.otp);
+    }).catch((e) => { alert(e) });
+    return false;
+}
+
+function connectWebsocket(otp) {
+    if (window["WebSocket"]) {
+        console.log("supports websockets");
+        conn = new WebSocket("wss://" + document.location.host + "/ws?otp=" + otp);
+
+        conn.onopen = function (evt) {
+            document.getElementById("connection-header").innerHTML = "Connected to Websocket: true";
+        }
+
+        conn.onclose = function (evt) {
+            document.getElementById("connection-header").innerHTML = "Connected to Websocket: false";
+        }
+
+        conn.onmessage = function (evt) {
+            const eventData = JSON.parse(evt.data);
+            const event = Object.assign(new Event, eventData);
+            routeEvent(event);
+        }
+
+    } else {
+        alert("Not supporting websockets");
+    }
+}
+
+window.onload = function () {
+    document.getElementById("chatroom-selection").onsubmit = changeChatRoom;
+    document.getElementById("chatroom-message").onsubmit = sendMessage;
+    document.getElementById("login-form").onsubmit = login;
+};
